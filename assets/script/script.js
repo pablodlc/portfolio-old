6c6674194fa5e2e3b82c7149fda328e57c7ecdef
pablo de la cruz <pablodlc@gmail.com>
2021-08-13 21:52:26 -0700

    init commit

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+// var ramonesImg = new Image;
+// // If wrong, src may = ./assets..."
+// ramonesImg.src = "Ramones.jpg"; "
+
+
+
+// var pieces = [
+//     {
+//         title: "Ramones Fanpage",
+//         image: "",
+//         text: "A vanilla HTML fanpage for the greatest band of all time. Ever.",
+//     }
+// ]
